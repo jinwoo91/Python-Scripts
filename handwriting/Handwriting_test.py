4d79908c508f9909e6bc3c71e343d028a6598d3b
Jinwoo Kang <92353871+jinwoo91@users.noreply.github.com>
2023-06-23 20:45:34 +0900

    longer multi-line text problem solved.

--- handwriting/Handwriting_test.py
+++ handwriting/Handwriting_test.py
@@ -15,8 +15,21 @@ def home():
 
 def generate_worksheet(text):
     lines = text.split('\n')
+    max_length = 90  # The maximum length of a line of text. Adjust as needed.
     line_height = 40  # the height of a line of text, adjust as needed
-    svg_height = line_height * len(lines)  # set the height of the svg
+    
+    # Split long lines
+    split_lines = []
+    for line in lines:
+        while len(line) > max_length:
+            split_point = line[:max_length].rfind(' ')
+            if split_point == -1:
+                split_point = max_length
+            split_lines.append(line[:split_point])
+            line = line[split_point:].lstrip()  # Remove leading white space
+        split_lines.append(line)
+    
+    svg_height = line_height * len(split_lines)  # set the height of the svg
 
     dwg = svgwrite.Drawing(profile='tiny', size=('100%', f'{svg_height}px'))  # set the height of the svg
     y = line_height
@@ -25,14 +38,14 @@ def generate_worksheet(text):
     y3 = 1
     y4 = 10
     y5 = 0
-    for line in lines:
-        dwg.add(dwg.line(start=(10, y+y1), end=(780, y+y1), stroke=svgwrite.rgb(0, 0, 0, "%"), stroke_opacity=0.3, stroke_dasharray="2 2"))  # ascender line
-        dwg.add(dwg.line(start=(10, y+y2), end=(780, y+y2), stroke=svgwrite.rgb(0, 0, 0, "%"), stroke_opacity=0.3, stroke_dasharray="2 2"))  # median line
-        dwg.add(dwg.line(start=(10, y+y3), end=(780, y+y3), stroke=svgwrite.rgb(150, 0, 0, "%"), stroke_opacity=0.6))  # descender line
-        dwg.add(dwg.line(start=(10, y+y4), end=(780, y+y4), stroke=svgwrite.rgb(0, 0, 0, "%"), stroke_opacity=0.3, stroke_dasharray="2 2"))  # base line
-        dwg.add(dwg.text(line, insert=(20, y+y5), fill="black", fill_opacity=0.3, font_size="20px", font_family="'Pacifico', cursive"))
-        dwg.add(dwg.line(start=(10, y+y1), end=(10, y+y3), stroke=svgwrite.rgb(150, 0, 0, "%"), stroke_opacity=0.6))  # left vertical line
-        dwg.add(dwg.line(start=(780, y+y1), end=(780, y+y3), stroke=svgwrite.rgb(150, 0, 0, "%"), stroke_opacity=0.6))  # right vertical line
+    for line in split_lines:
+        dwg.add(dwg.line(start=(10, y+y1), end=(780, y+y1), stroke=svgwrite.rgb(0, 0, 0, "%"), stroke_opacity=0.5, stroke_dasharray="2 2"))  # ascender line
+        dwg.add(dwg.line(start=(10, y+y2), end=(780, y+y2), stroke=svgwrite.rgb(0, 0, 0, "%"), stroke_opacity=0.5, stroke_dasharray="2 2"))  # median line
+        dwg.add(dwg.line(start=(10, y+y3), end=(780, y+y3), stroke=svgwrite.rgb(150, 0, 0, "%"), stroke_opacity=0.5))  # descender line
+        dwg.add(dwg.line(start=(10, y+y4), end=(780, y+y4), stroke=svgwrite.rgb(0, 0, 0, "%"), stroke_opacity=0.5, stroke_dasharray="2 2"))  # base line
+        dwg.add(dwg.text(line, insert=(20, y+y5), fill="grey", fill_opacity=0.8, font_size="20px", font_family="'Pacifico', cursive"))
+        dwg.add(dwg.line(start=(10, y+y1), end=(10, y+y3), stroke=svgwrite.rgb(150, 0, 0, "%"), stroke_opacity=0.5))  # left vertical line
+        dwg.add(dwg.line(start=(780, y+y1), end=(780, y+y3), stroke=svgwrite.rgb(150, 0, 0, "%"), stroke_opacity=0.5))  # right vertical line
         y += line_height  # adjust line spacing as needed
 
     firstline = lines[0].replace("\"","").replace("\r","")
